90a3c1015210b3607526668354283eac40bb7fb9
Halvor Linder <halvor_lh@hotmail.no>
2023-07-12 18:49:36 +0200

    solve 5 in Nim

---
+++ 5/A.nim
@@ -0,0 +1,32 @@
+import std/sugar
+import std/strutils
+
+let lines = collect(newSeq):
+  for line in lines "../Data/5.txt": line
+
+const LANES = 9
+const INITIAL_ROWS = 8
+const UNUSED_ROWS = 2
+
+var crates : array[LANES, seq[char]]
+
+for lane in 0..LANES-1:
+    crates[lane] = newSeq[char](0)
+    let offset = 4*lane + 1
+    for row in countdown(INITIAL_ROWS-1,0):
+        let crate = lines[row][offset];
+        if isAlphaAscii(crate):
+            crates[lane].add(lines[row][offset])
+
+for line in lines[INITIAL_ROWS+UNUSED_ROWS..lines.len-1]:
+    let tokens = split(line, " ")
+    let count = parseInt(tokens[1])
+    let source = parseInt(tokens[3])-1
+    let target = parseInt(tokens[5])-1
+    for _ in 1..count:
+        crates[target].add(crates[source][^1])
+        crates[source].delete(crates[source].len-1)
+
+for lane in crates: 
+    stdout.write lane[^1]
+echo ""
--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 | 2  | Zig    |
 | 3  | C    |
 | 4  | Prolog (SWI)    |
+| 5  | Nim    |
